fe6f8f56542f95e8bd3578d283ec86ca00a4bd5f
Vagner <vagnereix.dev@gmail.com>
2024-10-31 22:50:29 -0300

    feat: add startups card and setup Sanity

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/search-form';
+import StartupCard from '@/components/startup-card';
 
 export default async function Home({
   searchParams,
@@ -7,17 +8,47 @@ export default async function Home({
 }) {
   const q = (await searchParams).query;
 
+  const posts = [
+    {
+      _createdAt: new Date(),
+      _id: '1',
+      author: { _id: 1, name: 'Vagner' },
+      description: 'A platform for connecting with entrepreneurs',
+      image: 'https://github.com/vagnereix.png',
+      category: 'Tech',
+      title: 'Startup Connect',
+      views: 1000,
+    },
+  ];
+
   return (
-    <section className='pink_container'>
-      <h1 className='heading'>
-        Pitch your startup, <br /> connect with entrepreneurs!
-      </h1>
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>
+          Pitch your startup, <br /> connect with entrepreneurs!
+        </h1>
 
-      <p className='sub-heading !max-w-3xl'>
-        Submit Ideas, vote on pitches, and get noticed in virtual competitions.
-      </p>
+        <p className='sub-heading !max-w-3xl'>
+          Submit Ideas, vote on pitches, and get noticed in virtual
+          competitions.
+        </p>
 
-      <SearchForm query={q} />
-    </section>
+        <SearchForm query={q} />
+      </section>
+
+      <section className='section_container'>
+        <p className='text-30-semibold'>
+          {q ? `Search results for "${q}"` : `All startups`}
+        </p>
+
+        <ul className='mt-7 card_grid'>
+          {!!posts?.length ? (
+            posts.map((post) => <StartupCard key={post._id} post={post} />)
+          ) : (
+            <p className='no-results'>No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
---
+++ src/components/startup-card.tsx
@@ -0,0 +1,64 @@
+import { formatDate } from '@/lib/utils';
+import { EyeIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+
+interface StartupCardProps {
+  post: StartupTypeCard;
+}
+
+export default function StartupCard({
+  post: { _id, _createdAt, views, author, title, description, image, category },
+}: StartupCardProps) {
+  return (
+    <li className='startup-card group'>
+      <div className='flex-between'>
+        <p className='startup_card_date'>{formatDate(_createdAt)}</p>
+
+        <div className='flex gap-1.5'>
+          <EyeIcon className='size-6 text-primary' />
+          <span className='text-16-medium'>{views}</span>
+        </div>
+      </div>
+
+      <div className='flex-between mt-5 gap-5'>
+        <div className='flex-1'>
+          <Link href={`/user/${author?._id}`}>
+            <p className='text-16-medium line-clamp-1'>{author?.name}</p>
+          </Link>
+
+          <Link href={`/startup/${_id}`}>
+            <h3 className='text-26-semibold'>{title}</h3>
+          </Link>
+        </div>
+
+        <Link href={`/user/${author?._id}`}>
+          <Image
+            src='https://placehold.co/48x48'
+            alt='placeholder'
+            width={48}
+            height={48}
+            className='rounded-full'
+          />
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className='startup-card_desc'>{description}</p>
+
+        <img src={image} alt='Placeholder' className='startup-card_img' />
+      </Link>
+
+      <div className='flex-between gap-3 mt-5'>
+        <Link href={`/?query=${category.toLowerCase()}`}>
+          <p className='text-16-medium'>{category}</p>
+        </Link>
+
+        <Button className='startup-card_btn'>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </li>
+  );
+}
